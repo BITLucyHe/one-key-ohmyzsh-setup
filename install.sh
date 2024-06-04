a2e6e9683f6f597e96fe0c1c2593bafaead78bca
BITLucyHe <1072247581@qq.com>
2024-06-04 22:32:13 +0800

    简单安装了字体和插件

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+echo "安装powerline字体"
+echo "字体项目原地址：https://github.com/powerline/fonts"
+chmod +x fonts/install.sh && ./fonts/install.sh 
+
+echo "安装自动补全插件"
+echo "项目原地址：https://github.com/zsh-users/zsh-autosuggestions"
+cp -R zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions 
+
+echo "安装语法高亮插件"
+echo "项目原地址：https://github.com/zsh-users/zsh-syntax-highlighting"
+cp -R zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting 
