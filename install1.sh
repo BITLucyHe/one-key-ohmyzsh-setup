3551826c2380c5532d24dfa4cb5bd5ddccad5b1a
BITLucyHe <1072247581@qq.com>
2024-06-05 18:11:35 +0800

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,56 +1,98 @@
 #!/bin/sh
 
-# A Little ReadMe
-# This script is for the prerequisite equippment of oh-my-zsh.
+# 简介
+# 这个脚本是安装oh-my-zsh的先决条件，即安装zsh和相关包
 
+export LANG=en_US.UTF-8;
 
 # check the git
-echo "Check the location of the git"
+echo "检查是否安装git"
 which git &> /dev/null
 if [[ $? -ne 0 ]]; then
-	echo "You need to install a git.";
-	echo "Please run the command 'sudo apt install git'.";
+	echo "尚未安装git";
+	echo "正在为你安装";
+	echo "将要运行命令'sudo apt install git'";
+	sudo apt install git;
 else
-	echo "Your git is in $(which git).";
+	echo "你的git在 $(which git).";
 	echo "";
 fi;
 
 # check the wget
-echo "Check the location of the wget"
+echo "检查是否安装wget"
 which git &> /dev/null
 if [[ $? -ne 0 ]]; then
-	echo "You need to install a wget.";
-	echo "Please run the command 'sudo apt install wget'.";
+	echo "尚未安装wget.";
+	echo "将要运行命令'sudo apt install wget'";
+	sudo apt install wget;
 else
-	echo "Your wget is in $(which wget).";
+	echo "你的wget在 $(which wget).";
 	echo "";
 fi;
 
+# check the sys
+OS=$(uname -s);
+if [[ $OS == "Darwin" ]]; then
+	# Install a zsh for mac
+	echo "检查是否安装zsh"
+	which zsh &> /dev/null
+	if [[ $? -ne 0 ]]; then
+		echo "尚未安装zsh.";
+		echo "将要运行命令'brew install zsh'";
+		brew install zsh;
+		echo "正在把zsh设为默认shell";
+		chsh -s "$(which zsh)";
+		echo "请重新打开终端，然后你会看到zsh!";
+	else
+		echo "你的zsh在路径 $(which zsh).";
+		echo "";
+	fi;
+else
+	# Install a zsh for linux
+	echo "检查是否安装zsh"
+	which zsh &> /dev/null
+	if [[ $? -ne 0 ]]; then
+		echo "尚未安装zsh.";
+		echo "将要运行命令'sudo apt install zsh'";
+		sudo apt install zsh;
+		echo "正在把zsh设为默认shell";
+		chsh -s "$(which zsh)";
+		echo "请重新打开终端，然后你会看到zsh!";
+	else
+		echo "你的zsh在路径 $(which zsh).";
+		echo "";
+	fi;
+
+fi;
 
 # Install a zsh
-echo "Check the location of the zsh"
+echo "检查是否安装zsh"
 which zsh &> /dev/null
 if [[ $? -ne 0 ]]; then
-	if [[ $(uname) -e Linux]]
-	echo "You need to install a zsh.";
-	echo "Please run the command 'sudo apt install zsh' to install a zsh.";
-	echo 'And run the command "chsh -s $(which zsh)" to set Zsh as the default Shell.';
-	echo "Then you need to reboot your computer."
+	echo "尚未安装zsh.";
+	echo "将要运行命令'sudo apt install zsh'";
+	sudo apt install zsh;
+	echo "正在把zsh设为默认shell";
+	chsh -s "$(which zsh)";
+	echo "请重新打开终端，然后你会看到zsh!";
 else
-	echo "Your zsh is in $(which zsh).";
+	echo "你的zsh在路径 $(which zsh).";
 	echo "";
 fi;
 
-
-# check the zsh 
-shell=$(echo $SHELL)
-if echo $SHELL | grep -q "zsh"; then
-	echo "Zsh has already been the defalut Shell.";
+# set the zsh as the default shell
+if echo "$SHELL" | grep -q "zsh"; then
+	echo "zsh已经是默认shell";
 else
-	echo 'run the command "chsh -s $(which zsh)" to set Zsh as the default Shell.';
-	echo 'Remember that you need to reboot the terminal if you reset the default shell.';
-	
+	echo 'zsh尚未成为默认shell';
+	echo "正在把zsh设为默认shell";
+	chsh -s "$(which zsh)";
+	echo "请重新打开终端，然后你会看到zsh!";
 fi;
 
-
-
+# install oh-my-zsh
+if echo "$SHELL" | grep -q "zsh"; then
+	sh -c "$(wget -O- https://install.ohmyz.sh/)";
+else 
+	echo "你需要吧zsh设置为默认shell."
+fi;
